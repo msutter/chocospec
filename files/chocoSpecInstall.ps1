8b287e5ca99d58831d0fc7cc462f413d1258e5bf
Marc Sutter <marc.sutter@swisscom.com>
2015-05-02 09:52:54 +0200

    fix var scope

--- files/chocoSpecInstall.ps1
+++ files/chocoSpecInstall.ps1
@@ -5,34 +5,44 @@ try {
   $PackagePath = Split-Path -Parent (Split-Path -Parent $MyInvocation.MyCommand.Path)
 
   # Import chocolateyPkgHelpers Module
-  Import-Module -Name chocoHelpers
+  Import-Module -force -verbose -Name c:\powershell\chocohelpers\chocohelpers.psm1
 
-  # Load Package Datas
-  $ChocoPkgData = Get-ChocoPkgData -PackagePath $PackagePath
-  Write-Host "ChocoPkgData: ${ChocoPkgData}"
+  # Load Package variables (datas)
+  $ChocoData = Import-ChocoHelpersVariables -PackagePath "${PackagePath}"
+  foreach ($Var in $ChocoData.Keys) {
+      Write-Verbose "Importing variable $($ChocoData.$Var) in the local scope"
+
+      # Pathes with spaces workaround
+      if ($ChocoData.$Var -is [system.string]) {
+          New-Variable -Name $Var -Value "$($ChocoData.$Var)"
+      } else {
+          New-Variable -Name $Var -Value $ChocoData.$Var
+      }
+  }
 
   #------- EXECUTE BEFORE-INSTALL SCRIPT WHEN PRESENT ---------#
-  $BeforeInstallPath = "$($ChocoPkgData.ToolsPath)\chocolateyBeforeInstall.ps1"
+  $BeforeInstallPath = "${ToolsPath}\chocolateyBeforeInstall.ps1"
   if (Test-Path -Path "${BeforeInstallPath}") {
     Write-Verbose "Executing ${BeforeInstallPath}"
     & "${BeforeInstallPath}"
   }
 
-  #------- INSTALLATION SETUP (Only files) ---------#
-  Install-ChocoPkgFiles $ChocoPkgData
-  Install-ChocoPkgInstallers $ChocoPkgData
+  #------- INSTALLATION SETUP (Only files deployement here ) ---------#
+  Install-ChocoPkgFiles
 
-
-  #------- EXECUTE AFTER-INSTALL SCRIPT WHEN PRESENT ---------#
-  $AfterInstallPath = "$($ChocoPkgData.ToolsPath)\chocolateyAfterInstall.ps1"
+  #------- EXECUTE AFTER-INSTALL SCRIPT ---------#
+  $AfterInstallPath = "${ToolsPath}\chocolateyAfterInstall.ps1"
   if (Test-Path -Path "${AfterInstallPath}") {
     Write-Verbose "Executing ${AfterInstallPath}"
     & "${AfterInstallPath}"
+  } else {
+    # Default install of exe, msi, etc...
+    Install-ChocoPkgInstallers
   }
 
   #------- DONE -----------------------#
-  Write-ChocolateySuccess $ChocoPkgData.PackageId
+  Write-ChocolateySuccess $PackageId
 } catch {
-  Write-ChocolateyFailure $ChocoPkgData.PackageId $($_.Exception.Message)
+  Write-ChocolateyFailure $PackageId $($_.Exception.Message)
   throw
 }
--- files/chocoSpecUninstall.ps1
+++ files/chocoSpecUninstall.ps1
@@ -4,33 +4,45 @@ try {
   # Set the location of the package on disk
   $PackagePath = Split-Path -Parent (Split-Path -Parent $MyInvocation.MyCommand.Path)
 
-  # Include chocolateyPkgHelpers Module
-  Import-Module -Name chocoHelpers
+  # Import chocolateyPkgHelpers Module
+  Import-Module -force -verbose -Name c:\powershell\chocohelpers\chocohelpers.psm1
 
-  # Load Package Datas
-  $ChocoPkgData = Get-ChocoPkgData -PackagePath $PackagePath
+  # Load Package variables (datas)
+  $ChocoData = Import-ChocoHelpersVariables -PackagePath "${PackagePath}"
+  foreach ($Var in $ChocoData.Keys) {
+      Write-Verbose "Importing variable $($ChocoData.$Var) in the local scope"
+
+      # Pathes with spaces workaround
+      if ($ChocoData.$Var -is [system.string]) {
+          New-Variable -Name $Var -Value "$($ChocoData.$Var)"
+      } else {
+          New-Variable -Name $Var -Value $ChocoData.$Var
+      }
+  }
 
   #------- BEFORE UNINSTALL SCRIPT ---------#
-  $BeforeUninstallPath = "$($ChocoPkgData.ToolsPath)\chocolateyBeforeUninstall.ps1"
+  $BeforeUninstallPath = "${ToolsPath}\chocolateyBeforeUninstall.ps1"
   if (Test-Path -Path "${BeforeUninstallPath}") {
     Write-Verbose "Executing ${BeforeUninstallPath}"
     & "${BeforeUninstallPath}"
+  } else {
+    # Default uninstall of exe, msi, etc...
+    Uninstall-ChocoPkgUninstallers
   }
 
   #------- UNINSTALLATION SETUP ---------#
-  Uninstall-ChocoPkgUninstallers $ChocoPkgData
-  Uninstall-ChocoPkgFiles $ChocoPkgData
+  Uninstall-ChocoPkgFiles
 
   #------- AFTER UNINSTALL SCRIPT ---------#
-  $AfterUninstallPath = "$($ChocoPkgData.ToolsPath)\chocolateyAfterUninstall.ps1"
+  $AfterUninstallPath = "${ToolsPath}\chocolateyAfterUninstall.ps1"
   if (Test-Path -Path "${AfterUninstallPath}") {
     Write-Verbose "Executing ${AfterUninstallPath}"
     & "${AfterUninstallPath}"
   }
 
   #------- DONE -----------------------#
-  Write-ChocolateySuccess $ChocoPkgData.PackageId
+  Write-ChocolateySuccess $PackageId
 } catch {
-  Write-ChocolateyFailure $ChocoPkgData.PackageId $($_.Exception.Message)
+  Write-ChocolateyFailure $PackageId $($_.Exception.Message)
   throw
 }
