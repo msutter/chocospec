f4257fc4b28cae85cdfa51bd5d8527efe6b6786e
Marc Sutter <marc.sutter@swisscom.com>
2015-06-01 22:46:46 +0200

    remove manual import

--- files/chocoSpecUninstall.ps1
+++ files/chocoSpecUninstall.ps1
@@ -1,4 +1,2 @@
-#Import-Module -force -Name chocoHelpers
-Import-Module -force -verbose -Name c:\powershell\chocohelpers\chocohelpers.psm1
-Import-Module -force -verbose -Name c:\powershell\ZipParts\ZipParts.psm1
+Import-Module -force -Name chocoHelpers
 Invoke-ChocoBuildFeatures
