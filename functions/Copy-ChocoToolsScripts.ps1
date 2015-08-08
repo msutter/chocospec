4816517ca4540db911e73e6e71d54d76469a6c79
Marc Sutter <marc.sutter@swisscom.com>
2015-08-08 16:51:50 +0200

    fix copy long path with robocopy and renaming

--- functions/Copy-ChocoToolsScripts.ps1
+++ functions/Copy-ChocoToolsScripts.ps1
@@ -35,13 +35,15 @@ function Copy-ChocoToolsScripts
     if ($scriptKeys.Contains('chocolateyInstall')) {
         # $null = Copy-Item -Path "${FilesPath}\chocoSpecInstall.ps1" `
         #     -Destination "${AbsToolsDirectory}\chocolateyInstall.ps1"
-        $null = Robocopy "${FilesPath}" "${AbsToolsDirectory}" chocolateyInstall.ps1
+        $null = Robocopy "${FilesPath}" "${AbsToolsDirectory}" chocoSpecInstall.ps1
+        $null = Rename-Item -path "${AbsToolsDirectory}/chocoSpecInstall.ps1" -newname "${AbsToolsDirectory}/chocolateyInstall.ps1"
     }
 
     if ($scriptKeys.Contains('chocolateyUninstall')) {
         # $null = Copy-Item -Path "${FilesPath}\chocoSpecUninstall.ps1" `
         #     -Destination "${AbsToolsDirectory}\chocolateyUninstall.ps1"
-        $null = Robocopy "${FilesPath}" "${AbsToolsDirectory}" chocolateyUninstall.ps1
+        $null = Robocopy "${FilesPath}" "${AbsToolsDirectory}" chocoSpecUninstall.ps1
+        $null = Rename-Item -path "${AbsToolsDirectory}/chocoSpecUninstall.ps1" -newname "${AbsToolsDirectory}/chocolateyUninstall.ps1"
     }
 
 }
