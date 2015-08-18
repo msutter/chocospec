507e65af9a66c7b9918bad94adde234eda07b970
Marc Sutter <marc.sutter@swisscom.com>
2015-08-18 10:18:05 +0200

    CLOUDDOPE-974 - Add Get-LastTag function

---
+++ functions/Get-LastTag.ps1
@@ -0,0 +1,34 @@
+function Get-LastTag {
+    <#
+    .SYNOPSIS
+
+    Returns the last tag of the current git branch
+
+    .DESCRIPTION
+
+    Returns the last tag of the current git branch
+
+    #>
+    [CmdletBinding()]
+    Param
+    (
+        # Specifies the git repo path
+        [ValidateScript( { Test-Path($_) } )]
+        [Parameter(Mandatory = $true, Position = 0, ValueFromPipeline = $true )]
+        [string]$Path,
+        # Specifies the git binary command path
+        [ValidateScript( { Test-Path($_) } )]
+        [Parameter(Mandatory = $false, Position = 1, ValueFromPipeline = $true )]
+        [string]$GitCommand = 'C:\Program Files (x86)\Git\bin\git.exe',
+    )
+
+    if ([System.IO.Path]::IsPathRooted($Path)) {
+        $AbsPath = $Path
+    } else {
+        $AbsPath = Join-Path (Get-Location) $Path
+    }
+
+    $LastTag = & "${GitCommand}" -C ${AbsPath} git describe --abbrev=0 --tags
+
+    $LastTag
+}
