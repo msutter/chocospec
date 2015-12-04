d5f307a692bb244a1e5f09b6ad52ebc4b257323c
Marc Sutter <marc.sutter@swisscom.com>
2015-12-04 13:41:27 +0100

    add support for multiple sources

--- functions/Invoke-AutoPrep.ps1
+++ functions/Invoke-AutoPrep.ps1
@@ -15,86 +15,84 @@ See the %prep section below for more.
   (
     [Parameter(Mandatory = $false)]
     [switch] $D,
-
-    [Parameter(Mandatory = $false)]
-    [int] $SourceIndex = 0
   )
 
-  $source = $sources[$SourceIndex]
+  if (!$D) {
+    $null = Remove-Item -Force -Recurse $PackageBuildPath
+  }
 
-  switch ($source.type) {
-    httprepo {
+  foreach ($source in $sources){
+    switch ($source.type) {
+      httprepo {
 
-      $SourceFile = $source.file
-      $SourcePath = Join-Path "${SourcesPath}" $SourceFile
-      $SourceExtension = $SourcePath.split('.')[-1]
+        $SourceFile = $source.file
+        $SourcePath = Join-Path "${SourcesPath}" $SourceFile
+        $SourceExtension = $SourcePath.split('.')[-1]
 
-      if (!$D) {
-        $null = Remove-Item -Force -Recurse $PackageBuildPath
-      }
 
-      if ($SourceExtension -eq 'zip') {
+        if ($SourceExtension -eq 'zip') {
 
-        Write-Verbose "Source file is a zip archive"
-        [System.Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem") | Out-Null
-        $ArchivePath = $SourcePath
+          Write-Verbose "Source file is a zip archive"
+          [System.Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem") | Out-Null
+          $ArchivePath = $SourcePath
 
-        # Inspecting the zip file
-        $Zip = [System.IO.Compression.ZipFile]::OpenRead($ArchivePath)
+          # Inspecting the zip file
+          $Zip = [System.IO.Compression.ZipFile]::OpenRead($ArchivePath)
 
-        $EntriesBasePathes = @()
-        foreach ($ZipEntry in $Zip.Entries) {
-          # check basepath for each file
-          Write-Verbose "ZipEntry.FullName: $($ZipEntry.FullName)"
-          $EntriesBasePathes += $ZipEntry.FullName.split('/')[0]
-        }
+          $EntriesBasePathes = @()
+          foreach ($ZipEntry in $Zip.Entries) {
+            # check basepath for each file
+            Write-Verbose "ZipEntry.FullName: $($ZipEntry.FullName)"
+            $EntriesBasePathes += $ZipEntry.FullName.split('/')[0]
+          }
 
-        $BasePathes       = ($EntriesBasePathes | select -unique)
-        $BasePathIsUnique = $BasePathes.count -eq 1
-        $BasePathIsDir    = $BasePathes -is [System.IO.DirectoryInfo]
-        $BasePathisValid  = $BasePathes -eq $PackageDirectoryName
+          $BasePathes       = ($EntriesBasePathes | select -unique)
+          $BasePathIsUnique = $BasePathes.count -eq 1
+          $BasePathIsDir    = $BasePathes -is [System.IO.DirectoryInfo]
+          $BasePathisValid  = $BasePathes -eq $PackageDirectoryName
 
-        # Add Pathes infos
-        Write-Verbose "BasePathes: ${BasePathes}"
-        Write-Verbose "BasePathIsDir: ${BasePathIsDir}"
-        Write-Verbose "BasePathIsUnique: ${BasePathIsUnique}"
-        Write-Verbose "BasePathisValid: ${BasePathisValid}"
+          # Add Pathes infos
+          Write-Verbose "BasePathes: ${BasePathes}"
+          Write-Verbose "BasePathIsDir: ${BasePathIsDir}"
+          Write-Verbose "BasePathIsUnique: ${BasePathIsUnique}"
+          Write-Verbose "BasePathisValid: ${BasePathisValid}"
 
-        if (!$BasePathIsUnique -Or !$BasePathIsDir)  {
-          # Create the directory and unpack in it
-          Write-Verbose "Zip Content has no base directory ! Creating ${PackageDirectoryName} as base directory"
+          if (!$BasePathIsUnique -Or !$BasePathIsDir)  {
+            # Create the directory and unpack in it
+            Write-Verbose "Zip Content has no base directory ! Creating ${PackageDirectoryName} as base directory"
+            $null = New-Item -Force -ItemType Directory $PackageBuildPath
+            $null = [System.IO.Compression.ZipFile]::ExtractToDirectory($ArchivePath, $PackageBuildPath)
+
+          } elseIf ($BasePathIsUnique -and !$BasePathisValid) {
+            # Unpack the zip and rename the invalid base folder
+            Write-Verbose "Zip Content has an invalid base directory ! Renaming ${BasePathes} to ${PackageDirectoryName} as base directory"
+            [System.IO.Compression.ZipFile]::ExtractToDirectory($ArchivePath, $BuildPath)
+            Rename-Item (Join-Path $BuildPath $BasePathes) $PackageBuildPath
+
+          } elseif ($BasePathIsUnique -and $BasePathisValid) {
+            Write-Verbose "Zip Content has a valid base directory"
+            # Unpack (should create the PackageBuild Directory)
+            $null = [System.IO.Compression.ZipFile]::ExtractToDirectory($ArchivePath, $BuildPath)
+
+          }
+
+        } else {
+          Write-Verbose "Source file is not a zip archive"
           $null = New-Item -Force -ItemType Directory $PackageBuildPath
-          $null = [System.IO.Compression.ZipFile]::ExtractToDirectory($ArchivePath, $PackageBuildPath)
-
-        } elseIf ($BasePathIsUnique -and !$BasePathisValid) {
-          # Unpack the zip and rename the invalid base folder
-          Write-Verbose "Zip Content has an invalid base directory ! Renaming ${BasePathes} to ${PackageDirectoryName} as base directory"
-          [System.IO.Compression.ZipFile]::ExtractToDirectory($ArchivePath, $BuildPath)
-          Rename-Item (Join-Path $BuildPath $BasePathes) $PackageBuildPath
-
-        } elseif ($BasePathIsUnique -and $BasePathisValid) {
-          Write-Verbose "Zip Content has a valid base directory"
-          # Unpack (should create the PackageBuild Directory)
-          $null = [System.IO.Compression.ZipFile]::ExtractToDirectory($ArchivePath, $BuildPath)
-
+          $null = Copy-Item -Force -Recurse "${SourcePath}" "${PackageBuildPath}"
         }
+      } # httprepo
 
-      } else {
-        Write-Verbose "Source file is not a zip archive"
-        $null = New-Item -Force -ItemType Directory $PackageBuildPath
-        $null = Copy-Item -Force -Recurse "${SourcePath}" "${PackageBuildPath}"
-      }
-    } # httprepo
+      git {
+          Write-Verbose "Source is a git repo"
+          $RepoName = ($source.url.split('/')[-1]) -replace '.git$', ''
+          Write-Verbose "Git repo name: ${RepoName}"
 
-    git {
-        Write-Verbose "Source is a git repo"
-        $RepoName = ($source.url.split('/')[-1]) -replace '.git$', ''
-        Write-Verbose "Git repo name: ${RepoName}"
+          $SourcePath = Join-Path "${SourcesPath}" $RepoName
+          $null = New-Item -Force -ItemType Directory $PackageBuildPath
+          $null = Get-ChildItem "${SourcePath}" -Exclude .git | Copy-Item -Force -Recurse -Destination "${PackageBuildPath}"
+      } # git
 
-        $SourcePath = Join-Path "${SourcesPath}" $RepoName
-        $null = New-Item -Force -ItemType Directory $PackageBuildPath
-        $null = Get-ChildItem "${SourcePath}" -Exclude .git | Copy-Item -Force -Recurse -Destination "${PackageBuildPath}"
-    } # git
-
-  } # switch
+    } # switch
+  } # foreach $sources
 }
